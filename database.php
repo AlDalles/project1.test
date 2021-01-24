75c53f62423fff869a7acf7abb0ee55fecbe9d04
Allen <allen@localhost>
2021-01-24 18:07:40 +0200

    add database.php

---
+++ database.php
@@ -0,0 +1,5 @@
+<?php
+
+$connect=mysqli_connect('localhost','homestead','secret','homestead') or die ("Error".mysqli_error($connect));
+
+?>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include 'database.php';
 phpinfo();
 $a=10;
 $b=20;
